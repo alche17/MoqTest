65a063f6b47294950512a5ce643d2b77bec62063
alche17 <alche17@student.monash.edu>
2018-01-19 16:38:32 +1100

    Add files via upload

---
+++ ClassLibrary1/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnitTest.Interfaces
+{
+    public interface ISurvey
+    {
+        string Name { get; set; }
+        int RespondentCount { get; set; }
+        IList<string> Questions { get; }
+    }
+
+    public interface ISurveryRepository
+    {
+        IEnumerable<ISurvey> Surveys { get; }
+    }
+}
---
+++ UnitTestProject1/UnitTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ISurvey _survey1Mock = new Mock<ISurvey>();
+        }
+    }
+}
